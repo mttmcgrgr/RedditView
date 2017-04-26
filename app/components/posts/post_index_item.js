a3552842aaef66b93a86ef9b4eb9ccec4055b98c
Matt McGregor <mttmcgrgr@gmail.com>
2017-04-25 21:31:03 -0700

    post index item mapping

---
+++ app/components/posts/post_index_item.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { ScrollView, View, Text, Image, TouchableOpacity, StyleSheet, TouchableHighlight  } from 'react-native';
+
+
+class PostIndexItem extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render (){
+
+
+    return(
+      <View style={styles.backgroundColorText}>
+        <TouchableHighlight>
+          <Text style={styles.includeFontPaddingText}>
+            title
+          </Text>
+        </TouchableHighlight>
+        <Text style={styles.includeFontPaddingText}>
+          subreddit
+        </Text>
+        <Text style={styles.includeFontPaddingText}>
+          author
+        </Text>
+
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+
+  backgroundColorText: {
+    top: 100,
+    left: 5,
+    backgroundColor: 'rgba(100, 100, 100, 0.3)'
+  },
+  includeFontPaddingText: {
+    top: 200,
+    fontSize: 15,
+    backgroundColor: '#EEEEEE',
+    color: '#000000',
+    textAlignVertical: 'center',
+    alignSelf: 'center',
+  }
+});
+
+export default PostIndexItem;
